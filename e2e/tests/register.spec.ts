99ce425cdc1e11168b1e358ffd706ce97ebdc0e2
avanijagani96 <s3827022@student.rmit.edu.au>
2023-04-14 13:55:36 +1000

    Refined register e2e test

--- e2e/tests/register.spec.ts
+++ e2e/tests/register.spec.ts
@@ -2,72 +2,109 @@ import { test, expect } from '@playwright/test';
 
 const E2E_WRONG_EMAIL= "test"
 const E2E_EMAIL = "test@gmail.com"
+const E2E_USERNAME = "testuser"
 const E2E_PASSWORD = "test@1234"
+const E2E_SMALL_PASSWORD = "abc"
 
 test.describe('Register Page', () => {
   test.describe.configure({ mode: 'serial' });
 
   test.beforeEach(async ({ page }) => {
       await page.goto('/login');
-      await page.getByRole('button', { name: 'Register' }).click();
+      await page.getByText('Register now').click();
       await expect(page).toHaveURL('/register');
-      await expect(page).toHaveTitle(/Cultured UP App/);
+      await expect(page.getByRole('img', { name: 'Cultured Up All Logo' })).toBeVisible();
   });
 
   test('should render register page', async ({ page }) => {
+     await expect(page.getByText('Welcome!')).toBeVisible();
+
+     await expect(page.getByText('Enter your details to get started.')).toBeVisible();
+
      await expect(page.getByText("Email")).toBeVisible();
+
+     await expect(page.getByText("Username")).toBeVisible();
 
      await expect(page.getByText('Password', { exact: true })).toBeVisible();
 
-     await expect(page.getByText(/Confirm Password/)).toBeVisible();
+     await expect(page.getByText('Confirm Password')).toBeVisible();
+
+     await expect(page.getByText('I agree to the terms of service')).toBeVisible();
 
      await expect(page.getByRole('button', { name: 'Register' })).toBeVisible();
 
-     await expect(page.getByRole('button', { name: 'Forgot Password' })).toBeVisible();
+     await expect(page.getByText('Already have an account? Login')).toBeVisible();
 
-     await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
+     await expect(page.getByRole('img', { name: 'all hands in' })).toBeVisible();
   });
 
   test('should redirect to login page', async ({ page }) => {
-     await page.getByRole('button', { name: 'Login' }).click();
+     await page.getByText('Login').click();
 
      await expect(page).toHaveURL('/login');
   });
 
- test('should throw error on entering wrong credentials', async ({ page }) => {
-    await page.locator('input[type="text"]').fill(E2E_WRONG_EMAIL);
+ test('should throw error on entering wrong email', async ({ page }) => {
+    await page.getByPlaceholder('john@doe.com').fill(E2E_WRONG_EMAIL);
 
-    await page.getByRole('textbox').nth(1).fill(E2E_PASSWORD);
+    await page.getByPlaceholder('john doe').fill(E2E_USERNAME);
 
-    await page.getByRole('textbox').nth(2).fill(E2E_PASSWORD);
+    await page.getByPlaceholder('Minimum 6 characters.').fill(E2E_PASSWORD);
+
+    await page.getByPlaceholder('Make sure it matches!').fill(E2E_PASSWORD);
+
+    await page.getByRole('checkbox').click();
 
     await page.getByRole('button', { name: 'Register' }).click();
 
-    await expect(page.getByText(/Please check your email/)).toBeVisible();
+    await expect(page.getByText('Please check your email')).toBeVisible();
   });
 
   test('should throw error on entering unmatched passwords', async ({ page }) => {
-    await page.locator('input[type="text"]').fill(E2E_EMAIL);
+     await page.getByPlaceholder('john@doe.com').fill(E2E_EMAIL);
 
-    await page.getByRole('textbox').nth(1).fill(E2E_PASSWORD);
+    await page.getByPlaceholder('john doe').fill(E2E_USERNAME);
 
-    await page.getByRole('textbox').nth(2).fill(E2E_PASSWORD + " ");
+    await page.getByPlaceholder('Minimum 6 characters.').fill(E2E_PASSWORD);
+
+    await page.getByPlaceholder('Make sure it matches!').fill(E2E_PASSWORD + " ");
+
+    await page.getByRole('checkbox').click();
 
     await page.getByRole('button', { name: 'Register' }).click();
 
-    await expect(page.getByText(/Make sure your password match up!/)).toBeVisible();
+    await expect(page.getByText('Make sure your password match up!')).toBeVisible();
   });
 
+  test('should throw error on entering passwords that is less than 6 characters', async ({ page }) => {
+    await page.getByPlaceholder('john@doe.com').fill(E2E_EMAIL);
+
+    await page.getByPlaceholder('john doe').fill(E2E_USERNAME);
+
+    await page.getByPlaceholder('Minimum 6 characters.').fill(E2E_SMALL_PASSWORD);
+
+    await page.getByPlaceholder('Make sure it matches!').fill(E2E_SMALL_PASSWORD);
+
+    await page.getByRole('checkbox').click();
+
+    await page.getByRole('button', { name: 'Register' }).click();
+
+    await expect(page.getByText('Your password should at least be 6 characters long')).toBeVisible();
+  });
 
   test('should register with proper credentials', async ({ page }) => {
 
-    await page.locator('input[type="text"]').fill(E2E_EMAIL);
+    await page.getByPlaceholder('john@doe.com').fill(E2E_EMAIL);
 
-    await page.getByRole('textbox').nth(1).fill(E2E_PASSWORD);
+    await page.getByPlaceholder('john doe').fill(E2E_USERNAME);
 
-    await page.getByRole('textbox').nth(2).fill(E2E_PASSWORD);
+    await page.getByPlaceholder('Minimum 6 characters.').fill(E2E_PASSWORD);
 
-  //  await page.getByRole('button', { name: 'Register' }).click();
+    await page.getByPlaceholder('Make sure it matches!').fill(E2E_PASSWORD);
+
+    await page.getByRole('checkbox').click();
+
+   // await page.getByRole('button', { name: 'Register' }).click();
 
   //  await expect(page).toHaveURL('/login');
   });
