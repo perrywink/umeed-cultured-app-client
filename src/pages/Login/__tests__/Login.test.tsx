ec242f3d06ad90ebb477fe17fd334607717b9518
MadhuraKulkarni2910 <s3854198@student.rmit.edu.au>
2023-04-15 21:30:54 +1000

    FE component test 2 - Login Page
    Test if the component state changes are occuring

--- src/pages/Login/__tests__/Login.test.tsx
+++ src/pages/Login/__tests__/Login.test.tsx
@@ -5,9 +5,10 @@
 
 import React from "react";
 import AuthForm from "../Login";
-import { getByRole, render, screen } from "@testing-library/react";
+import { fireEvent, getByRole, render, screen } from "@testing-library/react";
 import {BrowserRouter as Router} from 'react-router-dom';
 import '@testing-library/jest-dom'
+import {log} from 'console'
 
 const mockedUsedNavigate = jest.fn();
 jest.mock('react-router-dom', () => ({
@@ -21,13 +22,33 @@ describe('AuthForm component', () => {
     });
 
     it('should render all the component onto the screen', () => {
-        const { getByLabelText, getByText } = render(<AuthForm />);
+        render(<AuthForm />);
         expect(screen.getByPlaceholderText('john@doe.com')).toBeInTheDocument();
         expect(screen.getByPlaceholderText('Minimum 6 characters.')).toBeInTheDocument();
         expect(screen.getByText('Remember me')).toBeInTheDocument();
         expect(screen.getByText('Forgot Password?')).toBeInTheDocument();
         expect(screen.getByText('Login')).toBeInTheDocument();
     });
+
+    it('updates the email state change on email input',() => {
+        render(<AuthForm />);
+        const emailInput=screen.getByPlaceholderText("john@doe.com") as HTMLElement
+        //console.log(emailInput.ariaValueText)
+        fireEvent.change(emailInput,{target:{value:"john@doe.com"}})
+        expect(emailInput.ariaValueText="john@doe.com")
+        //console.log(emailInput.ariaValueText)
+    });
+
+    it('updates the password state change on password input',() => {
+        render(<AuthForm />);
+        const passwordInput=screen.getByPlaceholderText('Minimum 6 characters.') as HTMLElement
+        //console.log(passwordInput.ariaValueText)
+        fireEvent.change(passwordInput,{target:{value:"johndoe123"}})
+        expect(passwordInput.ariaValueText="johndoe123")
+        //console.log(passwordInput.ariaValueText)
+    });
+
+
 
 });
 
