31aefbe753e9b212ae6190c3130eb9947fc28e14
MadhuraKulkarni2910 <s3854198@student.rmit.edu.au>
2023-04-16 21:40:38 +1000

    FE component test 3 - Login Page
    Updates the test for checking state change

--- src/pages/Login/__tests__/Login.test.tsx
+++ src/pages/Login/__tests__/Login.test.tsx
@@ -5,10 +5,12 @@
 
 import React from "react";
 import AuthForm from "../Login";
-import { fireEvent, getByRole, render, screen } from "@testing-library/react";
+import { fireEvent, getByRole, render, screen, waitFor } from "@testing-library/react";
 import {BrowserRouter as Router} from 'react-router-dom';
-import '@testing-library/jest-dom'
-import {log} from 'console'
+import '@testing-library/jest-dom';
+import {log} from 'console';
+import firebase from 'firebase/app';
+import { auth } from "../../../config/firebase";
 
 const mockedUsedNavigate = jest.fn();
 jest.mock('react-router-dom', () => ({
@@ -16,7 +18,19 @@ jest.mock('react-router-dom', () => ({
   useNavigate: () => mockedUsedNavigate,
 }));
 
+
+jest.mock("../../../config/firebase");
+// mock firebase auth functions
+// jest.mock("firebase/auth", () => ({
+//     signInWithEmailAndPassword: jest.fn(),
+//     auth: jest.fn().mockReturnThis(),
+// }));
+
 describe('AuthForm component', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
     it('should render the component onto the screen', () => {
         expect(true).toBeTruthy();
     });
@@ -31,24 +45,19 @@ describe('AuthForm component', () => {
     });
 
     it('updates the email state change on email input',() => {
-        render(<AuthForm />);
-        const emailInput=screen.getByPlaceholderText("john@doe.com") as HTMLElement
-        //console.log(emailInput.ariaValueText)
-        fireEvent.change(emailInput,{target:{value:"john@doe.com"}})
-        expect(emailInput.ariaValueText="john@doe.com")
-        //console.log(emailInput.ariaValueText)
+        const{getByPlaceholderText} = render(<AuthForm />);
+        const emailInput= getByPlaceholderText("john@doe.com") as HTMLInputElement;
+        fireEvent.change(emailInput, {target: {value: "john@doe.com"}});
+        expect(emailInput.value).toBe("john@doe.com");
     });
 
     it('updates the password state change on password input',() => {
-        render(<AuthForm />);
-        const passwordInput=screen.getByPlaceholderText('Minimum 6 characters.') as HTMLElement
-        //console.log(passwordInput.ariaValueText)
-        fireEvent.change(passwordInput,{target:{value:"johndoe123"}})
-        expect(passwordInput.ariaValueText="johndoe123")
-        //console.log(passwordInput.ariaValueText)
+        const{getByPlaceholderText} = render(<AuthForm />);
+        const passwordInput= getByPlaceholderText('Minimum 6 characters.') as HTMLInputElement;
+        fireEvent.change(passwordInput, {target: {value: "johndoe123"}});
+        expect(passwordInput.value).toBe("johndoe123");
+
     });
-
-
 
 });
 
