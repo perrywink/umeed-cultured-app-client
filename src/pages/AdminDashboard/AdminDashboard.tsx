4944dfa94c5075166b59a3155971482dec9cc66d
avanijagani96 <s3827022@student.rmit.edu.au>
2023-04-24 16:30:48 +1000

    Added route for admin dashboard

---
+++ src/pages/AdminDashboard/AdminDashboard.tsx
@@ -0,0 +1,26 @@
+import { auth } from "../../config/firebase";
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { Button } from "../../components";
+
+const AdminDashboard = () => {
+  const navigate = useNavigate();
+  const handleSignout = () => {
+    signOut(auth);
+    sessionStorage.clear();
+    navigate("/login");
+  };
+
+  return (
+    <div className='min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12'>
+      <div className='p-10 xs:p-0 mx-auto md:w-full md:max-w-md'>
+        <div className='px-5 py-7'>
+          Hello there! You're logged in as an admin!
+        </div>
+        <Button onClick={handleSignout}>Sign out</Button>
+      </div>
+    </div>
+  );
+};
+
+export default AdminDashboard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-export { default as Login } from './Login/Login'
-export { default as Register } from './Register/Register'
-export { default as Dashboard } from './Dashboard/Dashboard'
-export { default as ResetPassword } from './ResetPassword/ResetPassword'
-export { default as Onboarding } from './Onboarding/Onboarding'
+export { default as Login } from "./Login/Login";
+export { default as Register } from "./Register/Register";
+export { default as Dashboard } from "./Dashboard/Dashboard";
+export { default as AdminDashboard } from "./AdminDashboard/AdminDashboard";
+export { default as ResetPassword } from "./ResetPassword/ResetPassword";
+export { default as Onboarding } from "./Onboarding/Onboarding";
