239a7e645da466741ddd7aea4be022ea39bcd2ce
Sneha Debsikdar <s3795730@student.rmit.edu.au>
2023-05-01 18:19:25 +1000

    input button

--- src/pages/AdminDashboard/CreatePost.tsx
+++ src/pages/AdminDashboard/CreatePost.tsx
@@ -1,7 +1,6 @@
 import { auth, storage } from "../../config/firebase";
-import { signOut } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
-import { Button } from "../../components";
+// import { useNavigate } from "react-router-dom";
+import { Button, Input } from "../../components";
 import { useEffect, useState } from "react";
 import {
     ref,
@@ -10,14 +9,24 @@ import {
     listAll,
     list,
 } from "firebase/storage";
+import React from "react";
 
 const CreatePost = () => {
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
-    const [mediaUpload, setMediaUpload] = useState<Blob>();
+    const [mediaUpload, setMediaUpload] = useState<File | undefined>();
     const [mediaUrl, setMediaUrls] = useState<string[]>([])
 
     const mediaListRef = ref(storage, `${auth.currentUser?.uid}/`);
+
+    const selectFile = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { files } = event.target;
+        const selectedFiles = files as FileList;
+        setMediaUpload(selectedFiles?.[0]);
+        console.log(event.target.files);
+    };
+
+
     const uploadFile = () => {
         if (mediaUpload !== undefined) {
             const mediaRef = ref(storage, `${auth.currentUser?.uid}/${mediaUpload.name}`);
@@ -43,10 +52,12 @@ const CreatePost = () => {
     return (
         <div className='min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12'>
             <div className='p-10 xs:p-0 mx-auto md:w-full md:max-w-md'>
-                <div className='px-5 py-7'>
-                    Upload files
-                </div>
-                <Button onClick={uploadFile}>Upload</Button>
+    
+                <input
+                    type="file"
+                    onChange={(e) => { console.log(e.target.files)}}
+                />
+                <Button >Upload</Button>
             </div>
         </div>
     );
