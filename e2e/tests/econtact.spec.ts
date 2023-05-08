f327bf5873f6d051a75c58e8dc3f10f63d1aae7a
MadhuraKulkarni2910 <s3854198@student.rmit.edu.au>
2023-05-08 19:43:04 +1000

    Changed registration credentials
    Changed registration credentials

--- e2e/tests/econtact.spec.ts
+++ e2e/tests/econtact.spec.ts
@@ -1,44 +1,39 @@
-import { test, expect, Page } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-const e2e_econtact_email = "teste2eusergit@gmail.com"
-const e2e_econtact_username = "teste2eusergit"
-const e2e_econtact_password = "teste2eusergit"
+const e2e_econtact_email = "teste2eusergit2@gmail.com"
+const e2e_econtact_username = "teste2eusergit2"
+const e2e_econtact_password = "teste2eusergit2"
 const e2e_econtct_contactno = "0422222222"
 
 test.describe('Econtact Page', () => {
-  let page: Page;
-  test.describe.configure({ mode: 'serial' });
+    test.describe.configure({ mode: 'serial' });
+  
+    test.beforeEach(async ({ page }) => {
+        await page.goto('/login');
+        await page.getByText('Register now').click();
+        await expect(page).toHaveURL('/login#/register');
+        await expect(page.getByRole('img', { name: 'Cultured Up All Logo' })).toBeVisible();
 
-  test.beforeAll(async ({browser}) =>{
-    page = await browser.newPage(); //Create a new Page instance
-  });
+        //fill in all the details on the register page
+        await page.getByPlaceholder('john@doe.com').fill(e2e_econtact_email);
+        await page.getByPlaceholder('john doe').fill(e2e_econtact_username);
+        await page.getByPlaceholder('0412346789').fill(e2e_econtct_contactno);
+        await page.getByPlaceholder('Minimum 6 characters.').fill(e2e_econtact_password);
+        await page.getByPlaceholder('Make sure it matches!').fill(e2e_econtact_password);
+        await page.getByRole('checkbox').click();
 
-  test.beforeEach(async () => {
-    await page.goto('/login');
-    await page.getByText('Register now').click();
-    await expect(page).toHaveURL('/login#/register');
-    await expect(page.getByRole('img', { name: 'Cultured Up All Logo' })).toBeVisible();
+        //click on register
+        await page.getByRole('button', { name: 'Register' }).click();
 
-    //fill in all the details on the register page
-    await page.getByPlaceholder('john@doe.com').fill(e2e_econtact_email);
-    await page.getByPlaceholder('john doe').fill(e2e_econtact_username);
-    await page.getByPlaceholder('0412346789').fill(e2e_econtct_contactno);
-    await page.getByPlaceholder('Minimum 6 characters.').fill(e2e_econtact_password);
-    await page.getByPlaceholder('Make sure it matches!').fill(e2e_econtact_password);
-    await page.getByRole('checkbox').click();
+        // //check if the E-contact page is loaded
+        // await expect(page.getByText("A loved one's number")).toBeVisible();
 
-    //click on register
-    await page.getByRole('button', { name: 'Register' }).click();
-
-    //check if the E-contact page is loaded
-    await expect(page.getByText("A loved one's number")).toBeVisible();
-
-    //navigate to the econtact page
-    await expect(page).toHaveURL('login#/onboarding');
-  });
+        //navigate to the econtact page
+        await expect(page).toHaveURL('login#/onboarding');
+    });
 
 
-  test('should register with proper credentials and fill the emergency contact', async () => {
+  test('should register with proper credentials and fill the emergency contact', async ({ page }) => {
     // Fill in name field
     const nameInput = "Test Emergency Contact UserName";
     await page.getByPlaceholder('Jane Doe').fill(nameInput);
