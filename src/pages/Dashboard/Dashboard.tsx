496597402d220cd7ced6807696a86c4d313a022d
YiJieChuah <s3847905@student.rmit.edu.au>
2023-05-12 20:39:24 +1000

    tags filter added to dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,25 +1,29 @@
 import Nav from "../../components/Nav/Nav";
 
-import { PostItem, Spinner } from "../../components";
+import { PostItem, SelectTags, Spinner } from "../../components";
 import { useGetUserTags } from "../../api/user";
 import { useSearchPosts } from "../../api/post";
 import { useEffect, useState } from "react";
 import { IUserOnTags } from "../../types/UsersOnTags";
 import { IPostWithMedia } from "../../types/Post";
 import { Tag } from "../../types/Tag";
-import { ArrowDownCircleIcon, EllipsisHorizontalCircleIcon, FaceFrownIcon, NoSymbolIcon } from "@heroicons/react/24/outline";
+import {
+  ArrowDownCircleIcon,
+  EllipsisHorizontalCircleIcon,
+  FaceFrownIcon,
+  NoSymbolIcon,
+} from "@heroicons/react/24/outline";
 import { useGetTagWithId } from "../../api/tag";
 import { useInView } from "react-intersection-observer";
 import React from "react";
 import { Masonry } from "@mui/lab";
+import { toast } from "react-toastify";
 
 const Dashboard = () => {
-  const { data: userOnTags, isSuccess: getUserOnTagsSuccess } =
-    useGetUserTags();
   const [tagIds, setTagIds] = useState<number[]>([]);
   const [numCols, setNumCols] = useState<number>(5);
 
-  const { data: tags, isSuccess: getTagsSuccess } = useGetTagWithId(tagIds);
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
 
   const { ref, inView } = useInView();
 
@@ -32,13 +36,32 @@ const Dashboard = () => {
       setTagIds(result);
     }
   };
+
+  const { data: userOnTags, isSuccess: getUserOnTagsSuccess } =
+    useGetUserTags();
+  const { data: tags, isSuccess: getTagsSuccess } = useGetTagWithId(tagIds);
   const {
     data: posts,
     isSuccess: getPostsSuccess,
     hasNextPage,
+    refetch,
     isFetchingNextPage,
     fetchNextPage,
-  } = useSearchPosts("", tagIds);
+  } = useSearchPosts(
+    "",
+    selectedTags.map((t) => t.id)
+  );
+
+  useEffect(() => {
+    if (tags) {
+      setSelectedTags(tags);
+    }
+  }, [getTagsSuccess]);
+
+  useEffect(() => {
+    // if (!!selectedTags && selectedTags.length > 0)
+    refetch();
+  }, [selectedTags]);
 
   useEffect(() => parseTags(), [getUserOnTagsSuccess]);
 
@@ -50,60 +73,57 @@ const Dashboard = () => {
 
   const alterCols = () => {
     if (window.innerWidth >= 768) {
-      setNumCols(5)
+      setNumCols(5);
     } else {
-      setNumCols(2)
+      setNumCols(2);
     }
-  }
+  };
 
   useEffect(() => {
-    alterCols()
+    alterCols();
     window.addEventListener("resize", () => {
-      alterCols()
+      alterCols();
     });
     return () => {
       window.removeEventListener("resize", () => {});
     };
   }, []);
 
+  const renderEmptyState = () => {
+    if (!!posts && posts.pages[0]?.data?.length <= 0) {
+      return (
+        <div className="flex flex-col align-middle w-full h-full text-gray-400 mt-5 gap-2">
+          <FaceFrownIcon className="w-7 h-7 mx-3 md:mx-auto" />
+          <div className="mx-3 md:mx-auto">
+            Nothing to see here. Try adding more tags to view posts.
+          </div>
+        </div>
+      );
+    }
+  };
 
   return (
     <div className="min-h-screen">
       <Nav />
       <div className="bg-white flex flex-col mt-2 mx-8">
-        <div className="flex gap-2 my-4 items-end overflow-scroll scrollbar-hide">
-          {getTagsSuccess &&
-            tags.map((tag: Tag) => (
-              <div
-                key={tag.id}
-                className="text-gray-600 bg-gray-100 text-xs font-medium mr-2 px-2.5 py-0.5 rounded mb-2"
-              >
-                {tag.name}
-              </div>
-            ))}
+        <div className="mb-3">
+          <SelectTags selectedTagsState={{ selectedTags, setSelectedTags }} />
         </div>
+        {renderEmptyState()}
         <Masonry columns={numCols} spacing={2} className="w-full">
           {getPostsSuccess &&
             posts &&
-            posts.pages.map((page) => { 
+            posts.pages.map((page) => {
               return (
-              <React.Fragment key={page.pageBookmark}>
-                {page.data.map((post: IPostWithMedia) => {
-                  return <PostItem key={post.id} post={post} />;
-                })}
-              </React.Fragment>
-            )})}
+                <React.Fragment key={page.pageBookmark}>
+                  {page.data.map((post: IPostWithMedia) => {
+                    return <PostItem key={post.id} post={post} />;
+                  })}
+                </React.Fragment>
+              );
+            })}
         </Masonry>
       </div>
-      {getUserOnTagsSuccess && userOnTags?.length === 0 && (
-        <div className="flex flex-col align-middle w-full h-full text-gray-400 mt-5 gap-2">
-          <FaceFrownIcon className="w-7 h-7 mx-3 md:mx-auto" />
-          <div className="mx-3 md:mx-auto">
-            We customized your feed according to your interests. But ended up
-            with nothing...
-          </div>
-        </div>
-      )}
       <div className="w-full flex">
         <button
           ref={ref}
@@ -111,11 +131,15 @@ const Dashboard = () => {
           disabled={!hasNextPage || isFetchingNextPage}
           className="bg-gray-100 p-3 mx-auto animate-slide-in mt-1 mb-3 text-gray-500"
         >
-          {isFetchingNextPage
-            ? <EllipsisHorizontalCircleIcon className="animate-ping w-5 h-5"/>
-            : hasNextPage
-            ? <ArrowDownCircleIcon className="w-5 h-5"/>
-            : <div className="flex gap-3"><NoSymbolIcon className="w-5 h-5"/> Nothing left to see here.</div>}
+          {isFetchingNextPage ? (
+            <EllipsisHorizontalCircleIcon className="animate-ping w-5 h-5" />
+          ) : hasNextPage ? (
+            <ArrowDownCircleIcon className="w-5 h-5" />
+          ) : (
+            <div className="flex gap-3">
+              <NoSymbolIcon className="w-5 h-5" /> Nothing left to see here.
+            </div>
+          )}
         </button>
       </div>
     </div>
