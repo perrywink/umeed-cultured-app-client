7a857f99911011e389459c26444d98eb3dc5623f
avanijagani96 <s3827022@student.rmit.edu.au>
2023-05-12 16:34:50 +1000

    Created Table using useReactTable

--- src/pages/AdminDashboard/Components/AdminTable.tsx
+++ src/pages/AdminDashboard/Components/AdminTable.tsx
@@ -1,47 +1,164 @@
+import React from "react";
 import { Post } from "../../../types/Post";
+import {
+  Column,
+  Table as ReactTable,
+  PaginationState,
+  useReactTable,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  ColumnDef,
+  OnChangeFn,
+  flexRender,
+} from "@tanstack/react-table";
 
 interface Props {
-  tabData: [];
+  tabData: Post[];
 }
 
 const AdminTable = ({ tabData }: Props) => {
-  const tableContent = tabData || [];
+  const data = tabData || [];
+
+  const columns = React.useMemo<ColumnDef<Post>[]>(() => [
+    {
+      accessorKey: "title",
+      header: () => "Title",
+      footer: (props) => props.column.id,
+    },
+    {
+      accessorKey: "author",
+      header: () => "Author",
+      footer: (props) => props.column.id,
+    },
+    {
+      accessorKey: "status",
+      header: "Status",
+      footer: (props) => props.column.id,
+    },
+    {
+      accessorKey: "accept",
+      header: "",
+      footer: (props) => props.column.id,
+    },
+    {
+      accessorKey: "reject",
+      header: "",
+      footer: (props) => props.column.id,
+    },
+  ]);
 
   return (
-    <div>
-      {tableContent.map((content: Post) => (
-        <div className='grid md:grid-cols-5'>
-          <div>
-            <h3> {content.title}</h3>
-          </div>
-          <div>
-            <h3> {content.author}</h3>
-          </div>
-          <div>
-            <h3> {content.status}</h3>
-          </div>
+    <>
+      <Table
+        {...{
+          data,
+          columns,
+        }}
+      />
+    </>
 
-          {content.status == "REJECTED" && (
-            <div>
-              <button> {"Manage"}</button>
-            </div>
-          )}
+    // <div>
+    //   {tableContent.map((content: Post) => (
+    //     <div className='grid md:grid-cols-5'>
+    //       <div>
+    //         <h3> {content.title}</h3>
+    //       </div>
+    //       <div>
+    //         <h3> {content.author}</h3>
+    //       </div>
+    //       <div>
+    //         <h3> {content.status}</h3>
+    //       </div>
 
-          {content.status == "IN_REVIEW" && (
-            <div className='grid md:grid-cols-2'>
-              <div>
-                <button> {"Accept"}</button>
-              </div>
+    //       {content.status == "REJECTED" && (
+    //         <div>
+    //           <button> {"Manage"}</button>
+    //         </div>
+    //       )}
 
-              <div>
-                <button> {"Reject"}</button>
-              </div>
-            </div>
-          )}
-        </div>
-      ))}
-    </div>
+    //       {content.status == "IN_REVIEW" && (
+    //         <div className='grid md:grid-cols-2'>
+    //           <div>
+    //             <button> {"Accept"}</button>
+    //           </div>
+
+    //           <div>
+    //             <button> {"Reject"}</button>
+    //           </div>
+    //         </div>
+    //       )}
+    //     </div>
+    //   ))}
+    // </div>
   );
 };
+
+function Table({
+  data,
+  columns,
+}: {
+  data: Post[];
+  columns: ColumnDef<Post>[];
+}) {
+  const table = useReactTable({
+    data,
+    columns,
+    // Pipeline
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    //
+    debugTable: true,
+  });
+
+  return (
+    <div className='p-2'>
+      <div className='h-2' />
+      <table>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <th key={header.id} colSpan={header.colSpan}>
+                    {header.isPlaceholder ? null : (
+                      <div>
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                      </div>
+                    )}
+                  </th>
+                );
+              })}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => {
+            return (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => {
+                  return (
+                    <>
+                      <td key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </td>
+                    </>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
 
 export default AdminTable;
