5d0f00a2a30dbb2188c4a69251793dafbcfe117f
MadhuraKulkarni2910 <s3854198@student.rmit.edu.au>
2023-05-16 14:55:05 +1000

    corrected all tests
    Changes all tests to ensure all of them run

--- e2e/tests/login.spec.ts
+++ e2e/tests/login.spec.ts
@@ -8,51 +8,38 @@ test.describe('Login Page', () => {
   test.describe.configure({ mode: 'serial' });
 
   test.beforeEach(async ({ page }) => {
-      await page.goto('#/login');
-      await expect(page.getByRole('img', { name: 'Cultured Up All Logo' })).toBeVisible();
+    await page.goto('#/login');
+    await expect(page.getByRole('img', { name: 'Cultured Up All Logo' })).toBeVisible();
   });
 
-   test('should render login page', async ({ page }) => {
+  test('should render login page', async ({ page }) => {
      await expect(page.getByText('Hi Again!')).toBeVisible();
-
      await expect(page.getByText('Enter your details to continue.')).toBeVisible();
-
      await expect(page.getByText("Email")).toBeVisible();
-
      await expect(page.getByText('Password', { exact: true })).toBeVisible();
-
      await expect(page.getByText('Remember me')).toBeVisible();
-
      await expect(page.getByText('Forgot Password?')).toBeVisible();
-
      await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
-
      await expect(page.getByText('Don\'t have an account? Register now')).toBeVisible();
-
      await expect(page.getByRole('img', { name: 'all hands in' })).toBeVisible();
   });
 
   test('should throw error on entering wrong email', async ({ page }) => {
     await page.getByPlaceholder('john@doe.com').fill(E2E_WRONG_EMAIL);
-
     await page.getByPlaceholder('Minimum 6 characters.').fill(E2E_PASSWORD);
-
     await page.getByRole('checkbox').click();
-
     await page.getByRole('button', { name: 'Login' }).click();
 
     await expect(page.getByText('Please check your email')).toBeVisible();
   });
 
-    test('should throw error on entering wrong credentials', async ({ page }) => {
-    await page.getByPlaceholder('john@doe.com').fill(E2E_EMAIL);
-
-    await page.getByPlaceholder('Minimum 6 characters.').fill(E2E_PASSWORD);
-
+  test('should throw error on entering wrong credentials', async ({ page }) => {
+    await page.getByPlaceholder('john@doe.com').fill("teste2euser@gmail.com");
+    await page.getByPlaceholder('Minimum 6 characters.').fill("teste2e");
     await page.getByRole('checkbox').click();
-
     await page.getByRole('button', { name: 'Login' }).click();
 
     await expect(page.getByText('Incorrect username or password')).toBeVisible();
   });
+
 });
