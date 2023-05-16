45e955897d7acf141eb6c6c3180e898769e8bfcd
MadhuraKulkarni2910 <s3854198@student.rmit.edu.au>
2023-05-16 17:59:24 +1000

    Added tests for checking entire onboarding process
    Added tests for checking entire onboarding process and a few more login tests

--- e2e/tests/login.spec.ts
+++ e2e/tests/login.spec.ts
@@ -1,8 +1,9 @@
 import { test, expect } from '@playwright/test';
 
+const E2E_EMAIL = "teste2euser@gmail.com"
+const E2E_PASSWORD = "teste2euser"
 const E2E_WRONG_EMAIL= "test"
-const E2E_EMAIL = "test@gmail.com"
-const E2E_PASSWORD = "test@1234"
+const E2E_WRONG_PASSWORD= "test@1234"
 
 test.describe('Login Page', () => {
   test.describe.configure({ mode: 'serial' });
@@ -32,6 +33,16 @@ test.describe('Login Page', () => {
      await expect(page.getByRole('img', { name: 'all hands in' })).toBeVisible();
   });
 
+  test('should login with correct credentials', async ({ page }) => {
+    await page.getByPlaceholder('john@doe.com').fill(E2E_EMAIL);
+    await page.getByPlaceholder('Minimum 6 characters.').fill(E2E_PASSWORD);
+    await page.getByRole('checkbox').click();
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    //Check toast to see if user is logged in
+    await expect(page.getByText("You're logged in!")).toBeVisible();
+  });
+
   test('should throw error on entering wrong email', async ({ page }) => {
     await page.getByPlaceholder('john@doe.com').fill(E2E_WRONG_EMAIL);
 
@@ -44,10 +55,10 @@ test.describe('Login Page', () => {
     await expect(page.getByText('Please check your email')).toBeVisible();
   });
 
-    test('should throw error on entering wrong credentials', async ({ page }) => {
+    test('should throw error on entering wrong password', async ({ page }) => {
     await page.getByPlaceholder('john@doe.com').fill(E2E_EMAIL);
 
-    await page.getByPlaceholder('Minimum 6 characters.').fill(E2E_PASSWORD);
+    await page.getByPlaceholder('Minimum 6 characters.').fill(E2E_WRONG_PASSWORD);
 
     await page.getByRole('checkbox').click();
 
@@ -55,4 +66,37 @@ test.describe('Login Page', () => {
 
     await expect(page.getByText('Incorrect username or password')).toBeVisible();
   });
+
+  test('should throw error on missing email', async ({ page }) => {
+    //Enter password but skip email
+    await page.getByPlaceholder('Minimum 6 characters.').fill(E2E_PASSWORD);
+    await page.getByRole('checkbox').click();
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    await expect(page.getByText('All required fields are not filled up.')).toBeVisible();
+  });
+
+  test('should throw error on missing password', async ({ page }) => {
+    //Enter email but skip password
+    await page.getByPlaceholder('john@doe.com').fill(E2E_EMAIL);
+    await page.getByRole('checkbox').click();
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    await expect(page.getByText('All required fields are not filled up.')).toBeVisible();
+  });
+
+  test('should navigate to forgot password page', async({ page }) => {
+    await expect(page.getByText("Forgot Password?")).toBeVisible();
+    await page.getByText('Forgot Password?').click();
+
+    await expect(page).toHaveURL('login#/reset-password');
+  });
+
+  test('should navigate to register page', async({ page }) => {
+    await expect(page.getByText("Register now")).toBeVisible();
+    await page.getByText('Register now').click();
+
+    await expect(page).toHaveURL('login#/register');
+  });
+
 });
