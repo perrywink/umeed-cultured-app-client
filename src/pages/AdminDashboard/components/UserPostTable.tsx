ca0faf3d05d0b1d9818df25cca3f4bbb6e96cc54
avanijagani96 <s3827022@student.rmit.edu.au>
2023-05-30 14:45:05 +1000

    Added delete post functionaly in user post table

--- src/pages/AdminDashboard/components/UserPostTable.tsx
+++ src/pages/AdminDashboard/components/UserPostTable.tsx
@@ -11,9 +11,9 @@ import {
 } from "@tanstack/react-table";
 import { Button } from "../../../components";
 import TablePagination from "../components/TablePagination";
-import { useUpdatePost } from "../../../api/post";
+import { useDeletePost, useUpdatePost } from "../../../api/post";
 import { toast } from "react-toastify";
-import { PencilSquareIcon } from "@heroicons/react/24/solid";
+import { PencilSquareIcon, TrashIcon } from "@heroicons/react/24/solid";
 import { useNavigate } from "react-router-dom";
 import Modal from "../../../components/Modal/Modal";
 
@@ -23,6 +23,7 @@ interface Props {
 
 const UserPostTable = ({ tabData }: Props) => {
   const { mutate: updatePost } = useUpdatePost();
+  const { mutate: deletePost } = useDeletePost();
   const data = useMemo(() => tabData, [tabData]);
   const navigate = useNavigate();
 
@@ -35,7 +36,7 @@ const UserPostTable = ({ tabData }: Props) => {
       {
         accessorFn: "title",
         header: "Title",
-        cell: (info) => titleDisplay(info)
+        cell: (info) => titleDisplay(info),
       },
       {
         accessorKey: "author",
@@ -58,29 +59,43 @@ const UserPostTable = ({ tabData }: Props) => {
         id: "edit",
         cell: (props) => (
           <button onClick={() => handleEdit(props?.row?.original)}>
-            <PencilSquareIcon className="h-6 w-6 text-gray-500 hover:text-umeed-beige" />
+            <PencilSquareIcon className='h-6 w-6 text-gray-500 hover:text-umeed-beige' />
           </button>
+        ),
+      }),
+      columnHelper.display({
+        id: "delete",
+        cell: (props) => (
+          <Modal
+            icon={
+              <TrashIcon className='h-6 w-6 text-gray-500 hover:text-umeed-beige' />
+            }
+            title='Delete Post'
+            body={
+              <div>{`Are you sure you want to permanantly delete the post "${props?.row?.original?.title}" ?`}</div>
+            }
+            action='Delete'
+            onClick={() => handleDelete(props)}></Modal>
         ),
       }),
     ],
     [tabData]
   );
-  
+
   const titleDisplay = (rowData: any) => {
-    return(
+    return (
       <div
-          className="font-regular text-slate-600 underline cursor-pointer overflow-hidden max-w-md flex-nowrap text-ellipsis"
-          onClick={() => handleClick(rowData.row.original)}
-        >
-          {rowData.row.original.title}
-        </div>
-    )
-  }
+        className='font-regular text-slate-600 underline cursor-pointer overflow-hidden max-w-md flex-nowrap text-ellipsis'
+        onClick={() => handleClick(rowData.row.original)}>
+        {rowData.row.original.title}
+      </div>
+    );
+  };
 
   const statusDisplay = (rowData: any) => {
     if (rowData.getValue() === "APPROVED") {
       return (
-        <span className="bg-umeed-cyan text-gray-500 text-xs font-medium mr-2 px-2.5 py-0.5">
+        <span className='bg-umeed-cyan text-gray-500 text-xs font-medium mr-2 px-2.5 py-0.5'>
           {rowData.getValue()}
         </span>
       );
@@ -88,21 +103,20 @@ const UserPostTable = ({ tabData }: Props) => {
       return (
         <Modal
           icon={
-            <div className="bg-umeed-tangerine-100 text-umeed-tangerine-500 text-xs font-medium mr-2 px-2.5 py-0.5">
+            <div className='bg-umeed-tangerine-100 text-umeed-tangerine-500 text-xs font-medium mr-2 px-2.5 py-0.5'>
               {rowData.getValue()}{" "}
             </div>
           }
-          title="Rejection Reason:"
+          title='Rejection Reason:'
           body={<div>{rowData?.row?.original?.rejectDsc}</div>}
           action={""}
           onClick={() => {
             handleRejectClick(rowData?.row?.original);
-          }}
-        ></Modal>
+          }}></Modal>
       );
     } else {
       return (
-        <span className="bg-gray-200 text-gray-500 text-xs font-medium mr-2 px-2.5 py-0.5 ">
+        <span className='bg-gray-200 text-gray-500 text-xs font-medium mr-2 px-2.5 py-0.5 '>
           {"IN REVIEW"}
         </span>
       );
@@ -113,9 +127,8 @@ const UserPostTable = ({ tabData }: Props) => {
     if (rowData.status === "IN_REVIEW" || rowData.status === "REJECTED") {
       return (
         <Button
-          className="bg-umeed-cyan hover:bg-cyan-200 text-gray-600 px-5 py-1 rounded"
-          onClick={() => handleApproveClick(rowData)}
-        >
+          className='bg-umeed-cyan hover:bg-cyan-200 text-gray-600 px-5 py-1 rounded'
+          onClick={() => handleApproveClick(rowData)}>
           Approve
         </Button>
       );
@@ -129,22 +142,23 @@ const UserPostTable = ({ tabData }: Props) => {
       return (
         <Modal
           icon={
-            <div className="bg-umeed-tangerine-100 hover:bg-umeed-tangerine-300 text-gray-600 px-5 py-1 rounded text-center">
+            <div className='bg-umeed-tangerine-100 hover:bg-umeed-tangerine-300 text-gray-600 px-5 py-1 rounded text-center'>
               Reject
             </div>
           }
-          title="Give a reason"
+          title='Give a reason'
           body={
             <input
-              className="border border-gray-300 rounded-none p-2 text-md w-full text-gray-700 font-light outline-gray-300 placeholder:text-gray-400"
-              onChange={(event) => { rejectReason = event.target.value }}
+              className='border border-gray-300 rounded-none p-2 text-md w-full text-gray-700 font-light outline-gray-300 placeholder:text-gray-400'
+              onChange={(event) => {
+                rejectReason = event.target.value;
+              }}
             />
           }
-          action="Submit"
+          action='Submit'
           onClick={() => {
             handleRejectClick(rowData);
-          }}
-        ></Modal>
+          }}></Modal>
       );
     } else {
       return <></>;
@@ -160,7 +174,7 @@ const UserPostTable = ({ tabData }: Props) => {
   };
 
   const handleRejectClick = async (rowData: any) => {
-    console.log("reasonn",rejectReason)
+    console.log("reasonn", rejectReason);
     if (rejectReason === "") {
       toast.error("No rejection reason given. Aborting reject.");
       return;
@@ -181,6 +195,10 @@ const UserPostTable = ({ tabData }: Props) => {
     navigate(`/admin/post?postId=${rowData.id}`);
   };
 
+  const handleDelete = (data: any) => {
+    deletePost({ postId: data?.row?.original?.id });
+  };
+
   const table = useReactTable({
     data,
     columns,
@@ -192,22 +210,20 @@ const UserPostTable = ({ tabData }: Props) => {
   });
 
   return (
-    <div className="w-full text-gray-700">
-      <div className="overflow-scroll">
-        <table className="w-full table-auto my-10 border-collapse rounded-md border">
+    <div className='w-full text-gray-700'>
+      <div className='overflow-scroll'>
+        <table className='w-full table-auto my-10 border-collapse rounded-md border'>
           <thead>
             {table.getHeaderGroups().map((headerGroup) => (
               <tr
-                className="border-b bg-gray-50 text-left"
-                key={headerGroup.id}
-              >
+                className='border-b bg-gray-50 text-left'
+                key={headerGroup.id}>
                 {headerGroup.headers.map((header) => {
                   return (
                     <th
-                      className="py-4 px-12"
+                      className='py-4 px-12'
                       key={header.id}
-                      colSpan={header.colSpan}
-                    >
+                      colSpan={header.colSpan}>
                       {header.isPlaceholder ? null : (
                         <div>
                           {flexRender(
@@ -228,9 +244,8 @@ const UserPostTable = ({ tabData }: Props) => {
                 <tr key={row.id}>
                   {row.getVisibleCells().map((cell) => (
                     <td
-                      className="border-b border-gray-200 py-2 px-12"
-                      key={cell.id}
-                    >
+                      className='border-b border-gray-200 py-2 px-12'
+                      key={cell.id}>
                       {flexRender(
                         cell.column.columnDef.cell,
                         cell.getContext()
